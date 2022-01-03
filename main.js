2b0278809518e7dd05277caa3fe1cadfc2f20b28
Carla <91063081+cicu90@users.noreply.github.com>
2022-01-03 13:24:24 +0000

    add tasks

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 const tasks = document.getElementById("tasksBtn");
 const main = document.getElementById("main-content");
 
-var divMain = document.createElement("div");
-divMain.setAttribute("class", "divMain");
-divMain.innerHTML = "Hola aqui un div";
-main.appendChild(divMain);
+tasks.addEventListener("click", function(){
+
+  var divMain = document.createElement("div");
+  divMain.setAttribute("class", "divMain");
+  main.appendChild(divMain);
+  
+  var inputMain = document.createElement("input");
+  inputMain.setAttribute("id", "name");
+  inputMain.setAttribute("type", "text")
+  inputMain.setAttribute("placeholder", "Add your tasks here...");
+  divMain.appendChild(inputMain);
+  
+  var inputMain = document.createElement("input");
+  inputMain.setAttribute("id", "addTasks");
+  inputMain.setAttribute("type", "submit")
+  inputMain.setAttribute("value", "Add tasks");
+  divMain.appendChild(inputMain);
+})
+
+var btn = document.getElementById("addTasks");
+
+btn.addEventListener("click", function(){
+  var user = document.getElementById("name");
+  var contador = 0;
+  if(user.value == ""){
+    alert('add your tasks');
+    return false;
+  } else{
+    contador++;
+ 
+ console.log(contador);
+
+ var task_list = document.createElement("")
+  }
+});
+
 
 
 

780fe2e2ac13727ad97f359ca3004d2b40038ee6
Carla <91063081+cicu90@users.noreply.github.com>
2022-01-05 17:54:30 +0000

    localstorage

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
-//var config= require('./object.js')
+
 import TaskObject from './object.js'
 
-// (()=>{
-//   const nombreLocalStorage= localStorage.getItem('nombreUsuario');
-//   console.log(nombreLocalStorage);
-//   if(nombreLocalStorage ===null){
-//       const nombre='Ignacio';
-//       localStorage.setItem('nombreUsuario', nombre);
-//   }else{
-      
-//   }
-// })();
 
 $('#tasksBtn').on('click', function(){
+  if($('#dinamic').children()== 0){ //revisar que solo se imprima una vez el input
+    return;
+  } else {
   $('#dinamic').append('<input id="name" type="text" placeholder="Task" class="form-control my-2"><input id="description" type="text" placeholder="Description task" class="form-control my-2"><input type="checkbox" id="important" name="state" value="importantV"> <label for="important">Important</label><input type="checkbox" id="complet" name="state" value="completV"> <label for="complete">Complete</label><button id="btnAddTask" class="btn btn-success" type="reset" >New Task</button>');
+}
   $("#btnAddTask").on("click", capture);
-})
+});
 var newTask=new TaskObject();
 
 const dataBase=[];
 
-
 function capture(){
+
+  var titleCapture = $('#name').val();
+  var descCapture = $('#description').val();
+
+
+  if(!titleCapture && !descCapture){
+  alert("Please fill out the task");
+  return;
+
+} else {
   var titleCapture = $('#name').val();
   var descCapture = $('#description').val();
   newTask = new TaskObject(titleCapture, descCapture);
   newTask = fillImportantAndcomplete(newTask);
 
+
   addDB();
 
   showTaskList(newTask);
-};
+}};
 
 function fillImportantAndcomplete(taskObject){
   var selected = $('input[name=state]:checked');
+  
   selected.each(function(){
     if($(this).val()=='importantV')
     {
@@ -48,7 +53,21 @@ function fillImportantAndcomplete(taskObject){
 function addDB(){
   dataBase.push(newTask);
   console.log(dataBase);
+ localStorage.setItem("name", JSON.stringify(newTask));
+
+  saveLocalStorages()
 };
+
+function saveLocalStorages() {
+
+  if(localStorage.getItem("name")==null) {
+    console.log("no hay nada exist");
+
+  }else{ //que no se sobreescriba falta
+    var name = JSON.parse(localStorage.getItem("name"));
+    console.log(name);
+    }
+}
 
 function showTaskList(taskObject){
   $('#taskList').append('<details>'+
